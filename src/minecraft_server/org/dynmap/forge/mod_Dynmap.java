62495562229d3a03351c6a01a7e3f9651cd57995
Mike Primm <mike@primmhome.com>
2012-11-21 15:34:20 -0600

    Update to 1.2-alpha-1: Update to 1.2 core

--- src/minecraft_server/org/dynmap/forge/mod_Dynmap.java
+++ src/minecraft_server/org/dynmap/forge/mod_Dynmap.java
@@ -16,7 +16,7 @@ import cpw.mods.fml.common.Mod.PreInit;
 
 public class mod_Dynmap extends BaseMod
 {
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2-alpha-1";
 
     public static DynmapPlugin plugin;
 
