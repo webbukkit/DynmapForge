4b3d847648b3bc8cd9cc6c57c6b3ca61eb3753e3
Mike Primm <mike@primmhome.com>
2012-12-27 22:20:30 -0600

    Fix WorldUpdateTracker on reloaded worlds

--- src/minecraft/org/dynmap/forge/DynmapPlugin.java
+++ src/minecraft/org/dynmap/forge/DynmapPlugin.java
@@ -1180,6 +1180,11 @@ public class DynmapPlugin
 	           	last_fworld = fw;
            		if(fw.isLoaded() == false) {
        				fw.setWorldLoaded(w);
+       				// Add tracker
+       	    		WorldUpdateTracker wit = new WorldUpdateTracker();
+       	    		wit.worldid = fw.getName();
+       	    		updateTrackers.put(fw.getName(), wit);
+       	    		w.addWorldAccess(wit);
            		}
     			return fw;
     		}
@@ -1189,6 +1194,7 @@ public class DynmapPlugin
     		/* Add to list if not found */
     		fw = new ForgeWorld(w);
     		worlds.put(fw.getName(), fw);
+    		// Add tracker
     		WorldUpdateTracker wit = new WorldUpdateTracker();
     		wit.worldid = fw.getName();
     		updateTrackers.put(fw.getName(), wit);
