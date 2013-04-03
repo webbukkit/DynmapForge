30a02f95677899c6b5f1b830b9aed6cc5ac6eccf
Mike Primm <mike@primmhome.com>
2013-04-02 23:00:26 -0500

    Handle earlier initialization when bukkit plugins pulled in before normal server mod initialization is complete

--- common/org/dynmap/forge/Proxy.java
+++ common/org/dynmap/forge/Proxy.java
@@ -9,9 +9,12 @@ public class Proxy
     {
     }
 	public DynmapPlugin startServer() {
-		DynmapPlugin plugin = new DynmapPlugin();
-		plugin.onEnable();
-		return plugin;
+	    DynmapPlugin plugin = DynmapPlugin.plugin;
+	    if(plugin == null) {
+	        plugin = new DynmapPlugin();
+	        plugin.onEnable();
+	    }
+        return plugin;
 	}
 	public void stopServer(DynmapPlugin plugin) {
 		plugin.onDisable();
