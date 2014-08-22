0fe522b431374f3889c6261b6af605fcbb57e14b
Mike Primm <mike@primmhome.com>
2014-08-22 00:36:03 -0500

    Fix SideOnly(SERVER) references that break single player mode

--- src/main/java/org/dynmap/forge/DynmapPlugin.java
+++ src/main/java/org/dynmap/forge/DynmapPlugin.java
@@ -510,7 +510,11 @@ public class DynmapPlugin
         @Override
         public String getServerName()
         {
-        	String sn = server.getServerHostname();
+            String sn;
+            if (server.isSinglePlayer())
+                sn = "Integrated";
+            else
+                sn = server.getServerHostname();
         	if(sn == null) sn = "Unknown Server";
         	return sn;
         }
@@ -931,7 +935,10 @@ public class DynmapPlugin
         
         @Override
         public String getServerIP() {
-            return server.getServerHostname();
+            if (server.isSinglePlayer())
+                return "0.0.0.0";
+            else
+                return server.getServerHostname();
         }
         @Override
         public File getModContainerFile(String name) {
